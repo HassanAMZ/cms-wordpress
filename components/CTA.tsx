9c6619587a7e1c27d4fd8adc061d4262e4865cee
Shahzada Ali Hassan <59549481+HassanAMZ@users.noreply.github.com>
2022-10-31 01:12:01 +0500

    Updated CTA and made it dymaic

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -2,28 +2,32 @@ import Link from "next/link";
 import { M_PLUS_Rounded_1c } from "@next/font/google";
 const mPlusRounded1C700 = M_PLUS_Rounded_1c({ weight: "700" });
 
-export default function CTA() {
+export default function CTA({
+  header,
+  headIcon,
+  headline,
+  caption,
+  cta,
+  ctaLink,
+  ctaIcon,
+}) {
   return (
     <div className="w-[450px]  border-gray-900 border-2 bg-gray-100">
       <div className="font-semibold p-4 border-b-2 border-gray-900 flex justify-between">
-        <div>Branding</div>
-        <div>{"o o o"}</div>
+        <div>{header}</div>
+        <div>{headIcon}</div>
       </div>
       <div className="px-4 py-6 space-y-4 flex flex-col jusitfy-center">
         <h1
           className={`leading-10 text-4xl tracking-tighter ${mPlusRounded1C700.className}`}
         >
-          Add value on your brand to make your product closer to your customers.
+          {headline}
         </h1>
-        <div className="text-md text-gray-700">
-          We are ready to help you crafting packages branding for your products
-          accoruding to your atrget amrkets. we helped more tha 1000 products in
-          United staes.
-        </div>
+        <div className="text-md text-gray-700">{caption}</div>
       </div>
       <div className="font-semibold text-xl p-4 border-t-2 border-gray-900 flex justify-between">
-        <Link href="/lets-talk">Let's Talk</Link>
-        <Link href="/lets-talk">{"->"}</Link>
+        <Link href={ctaLink}>{cta}</Link>
+        <Link href={ctaLink}>{ctaIcon}</Link>
       </div>
     </div>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,20 @@
 import Link from "next/link";
 import CTA from "./CTA";
 export default function Hero() {
+  const data = {
+    header: "Branding",
+    headIcon: "o o o",
+    headline:
+      " Add value on your brand to make your product closer to your customers.",
+    caption:
+      "We are ready to help you crafting packages branding for your products  accoruding to your atrget amrkets. we helped more tha 1000 products in   United staes.",
+    cta: "Let's Talk",
+    ctaLink: "/lets-talk",
+    ctaIcon: "->",
+  };
   return (
     <section className="flex p-10 bg-no-repeat bg-cover bg-[#CAF1EE]">
-      <CTA />
+      <CTA {...data} />
     </section>
   );
 }
